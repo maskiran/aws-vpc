af5558603c733825aef4a7c719aa4b011c9ea4db
Kiran Madabhushi <maskiran@gmail.com>
2021-03-26 00:01:48 -0700

    get vpc item api call added

--- api/apiresources/vpc.py
+++ api/apiresources/vpc.py
@@ -8,3 +8,9 @@ app = Blueprint('vpc', __name__)
 @app.route('', strict_slashes=False)
 def vpc_list():
     return db.get_items(models.Vpc, **request.args)
+
+
+@app.route('/<resource_id>')
+def vpc(resource_id):
+    item = db.get_item(models.Vpc, resource_id=resource_id)
+    return item
