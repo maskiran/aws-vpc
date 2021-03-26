3ccacb8fdb12bf66bbde55ac9b1908739642b67b
Kiran Madabhushi <maskiran@gmail.com>
2021-03-26 00:08:26 -0700

    recent vpcs and dashboard support

--- ui/src/routes.js
+++ ui/src/routes.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import VpcList from './vpc_list';
-import SubnetList from './subnet_list';
-import RouteTableList from './route_table_list';
-import SecurityGroupList from './security_group_list';
-import InstanceList from './instance_list';
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import VpcList from './vpc_list'
+import RecentVpcList from './recent_vpc_list'
+import SubnetList from './subnet_list'
+import RouteTableList from './route_table_list'
+import SecurityGroupList from './security_group_list'
+import InstanceList from './instance_list'
+import VpcDashboard from './vpc_dashboard'
 
 
 const Routes = (
@@ -13,6 +15,8 @@ const Routes = (
             <Redirect to="/vpcs" />
         </Route>
         <Route exact path="/vpcs" component={VpcList} />
+        <Route exact path="/recentvpcs" component={RecentVpcList} />
+        <Route exact path="/vpcdashboard" component={VpcDashboard} />
         <Route exact path="/subnets" component={SubnetList} />
         <Route exact path="/route-tables" component={RouteTableList} />
         <Route exact path="/security-groups" component={SecurityGroupList} />
