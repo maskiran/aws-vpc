29331dcc54395021bb53b3b61ed2e374a57d8058
Kiran Madabhushi <maskiran@gmail.com>
2021-03-26 00:03:51 -0700

    wrapper script to sync all aws resource

--- api/aws_resources/sync_aws.py
+++ api/aws_resources/sync_aws.py
@@ -2,8 +2,21 @@ from vpc import sync_vpcs
 from subnet import sync_subnets
 from route_table import sync_route_tables
 from security_group import sync_security_groups
+from instance import sync_instances
+from classic_lb import sync_clbs
+from elastic_lb import sync_elbs
 
+print('Sync VPCs')
 sync_vpcs()
+print('Sync Subnets')
 sync_subnets()
+print('Sync Route Tables')
 sync_route_tables()
+print('Sync Security Groups')
 sync_security_groups()
+print('Sync Instances')
+sync_instances()
+print('Sync Classic LBs')
+sync_clbs()
+print('Sync Network LBs')
+sync_elbs()
