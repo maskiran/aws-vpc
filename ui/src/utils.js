f15d49f056db622371fdbf5d06227a2b063e1487
Kiran Madabhushi <maskiran@gmail.com>
2021-03-26 00:08:54 -0700

    utils modified

--- ui/src/utils.js
+++ ui/src/utils.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { Tooltip, Button, Descriptions } from 'antd';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import { CopyOutlined, InfoCircleOutlined } from '@ant-design/icons';
-import { HiOutlineExternalLink } from 'react-icons/hi';
-import { SiAmazonaws, SiMicrosoftazure, SiGooglecloud } from 'react-icons/si';
+import React from 'react'
+import { Tooltip, Button, Typography } from 'antd'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+import { CopyOutlined, InfoCircleOutlined } from '@ant-design/icons'
+import { HiOutlineExternalLink } from 'react-icons/hi'
+import qs from 'query-string'
 
 export const trimField = (text, options) => {
     if (!text) return null;
@@ -29,91 +29,19 @@ export const trimField = (text, options) => {
     )
 }
 
-export const vpcLink = (vpcId, region) => {
-    var url = `https://console.aws.amazon.com/vpc/home?region=${region}#VpcDetails:VpcId=${vpcId}`
-    return <CopyToClipboard text={url}><span>
-        <a href={url} target="_blank" rel="noopener noreferrer">
-            {vpcId}
-            <HiOutlineExternalLink className="react-icon-right" />
-        </a>
-        <Button type="link" icon={<CopyOutlined />} />
-    </span></CopyToClipboard >
-}
-
-export const subnetLink = (subnetId, region) => {
-    var url = `https://console.aws.amazon.com/vpc/home?region=${region}#SubnetDetails:subnetId=${subnetId}`
-    return <CopyToClipboard text={url}><span>
-        <a href={url} target="_blank" rel="noopener noreferrer">
-            {subnetId}
-            <HiOutlineExternalLink className="react-icon-right" />
-        </a>
-        <Button type="link" icon={<CopyOutlined />} />
-    </span></CopyToClipboard >
-}
-
-export const securityGroupLink = (sgId, region) => {
-    var url = `https://console.aws.amazon.com/vpc/home?region=${region}#SecurityGroup:groupId=${sgId}`
-    return <CopyToClipboard text={url}><span>
-        <a href={url} target="_blank" rel="noopener noreferrer">
-            {sgId}
-            <HiOutlineExternalLink className="react-icon-right" />
-        </a>
-        <Button type="link" icon={<CopyOutlined />} />
-    </span></CopyToClipboard >
-}
-
-export const iamRoleLink = (roleName, region) => {
-    var url = `https://console.aws.amazon.com/iam/home?region=${region}#/roles/${roleName}`
-    return <CopyToClipboard text={url}><span>
-        <a href={url} target="_blank" rel="noopener noreferrer">
-            {roleName}
-            <HiOutlineExternalLink className="react-icon-right" />
-        </a>
-        <Button type="link" icon={<CopyOutlined />} />
-    </span></CopyToClipboard >
-}
-
-export const instanceLink = (instanceId, region) => {
-    var url = `https://console.aws.amazon.com/ec2/v2/home?region=${region}#InstanceDetails:instanceId=${instanceId}`
-    return <CopyToClipboard text={url}><span>
-        <a href={url} target="_blank" rel="noopener noreferrer">
-            {instanceId}
-            <HiOutlineExternalLink className="react-icon-right" />
-        </a>
-        <Button type="link" icon={<CopyOutlined />} />
-    </span></CopyToClipboard >
-}
-
-export const lbLink = (lbName, region, shortName) => {
-    var displayName = lbName;
-    if (shortName === true) {
-        var cmps = lbName.split(':');
-        displayName = cmps[cmps.length - 1]
-    } else if (shortName) {
-        // any other string provided
-        displayName = shortName;
-    }
-    var url = `https://console.aws.amazon.com/ec2/v2/home?region=${region}#LoadBalancers:search=${lbName};sort=loadBalancerName`
-    return <CopyToClipboard text={url}><span>
-        <a href={url} target="_blank" rel="noopener noreferrer">
-            {displayName}
-            <HiOutlineExternalLink className="react-icon-right" />
-        </a>
-        <Button type="link" icon={<CopyOutlined />} />
-    </span></CopyToClipboard >
-}
-
 export const linkInNewTab = (url, displayName) => {
     if (!displayName) {
         displayName = url;
     }
-    return <CopyToClipboard text={url}><span>
-        <a href={url} target="_blank" rel="noopener noreferrer">
-            {displayName}
-            <HiOutlineExternalLink className="react-icon-right" />
-        </a>
-        <Button type="link" icon={<CopyOutlined />} />
-    </span></CopyToClipboard >
+    return <CopyToClipboard text={url}>
+        <span>
+            <a href={url} target="_blank" rel="noopener noreferrer">
+                {displayName}
+                <HiOutlineExternalLink className="react-icon-right" />
+            </a>
+            <Button type="link" icon={<CopyOutlined />} />
+        </span>
+    </CopyToClipboard >
 }
 
 export const getResponseErrorMessage = (axiosErr) => {
@@ -129,30 +57,47 @@ export const getResponseErrorMessage = (axiosErr) => {
     return errMsg
 }
 
-export const renderRowAsDescription = (record, tableCols) => {
-    // from antd table cols structure, render a record as a descriptions
-    // table. this is for itemviewer details where there is no additional
-    // data
-    var items = [];
-    var columns = [];
-    tableCols.forEach(column => {
-        if (column.children) {
-            columns = columns.concat(column.children)
-        } else {
-            columns.push(column)
-        }
-    })
-    columns.forEach(column => {
-        var dItem = <Descriptions.Item label={column.title} key={column.title}>
-            {column.render ?
-                column.render(record[column.dataIndex], record)
-                :
-                record[column.dataIndex]}
-        </Descriptions.Item>
-        items.push(dItem);
-    })
-    return <Descriptions size="small" column={1} bordered
-        className="description-label-fit">
-        {items}
-    </Descriptions>
+export const vpcFilter = (urlSearch, stringify = "vpc_id") => {
+    // is vpc_id=value defined in the urlSearch
+    var params = qs.parse(urlSearch)
+    if (!params.vpc_id) {
+        return ""
+    }
+    if (stringify !== false) {
+        return "?" + qs.stringify({ [stringify]: params.vpc_id })
+    } else {
+        return params.vpc_id
+    }
+}
+
+export const Copy = (props) => {
+    /* 
+    props:
+    text => text to show and copy
+    tooltip => tooltip to show and copy
+    maincopy={false} => dont show copy for the main text
+    tooltipcopy={false} => dont show copy for the tooltip text
+    */
+    var mainCopyable = {
+        text: props.text,
+        tooltips: false
+    }
+    if (props.maincopy === false) {
+        mainCopyable = false
+    }
+    var tooltipCopyable = {
+        text: props.tooltip,
+        tooltips: false
+    }
+    if (props.tooltipcopy === false) {
+        tooltipCopyable = false
+    }
+    if (props.tooltip) {
+        var title = <Typography.Text copyable={tooltipCopyable}>{props.tooltip}</Typography.Text>
+        return <Tooltip title={title} color="white">
+            <Typography.Text copyable={mainCopyable}>{props.text}</Typography.Text>
+        </Tooltip>
+    } else {
+        return <Typography.Text copyable={mainCopyable}>{props.text}</Typography.Text>
+    }
 }
