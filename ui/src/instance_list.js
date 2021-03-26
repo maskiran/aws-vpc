f2173f14be20cd3677f36d8805da2df79804351f
Kiran Madabhushi <maskiran@gmail.com>
2021-03-26 00:06:48 -0700

    updates to instance list

--- ui/src/instance_list.js
+++ ui/src/instance_list.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import _ from 'lodash';
-import ItemsList from 'react-antd-itemslist';
-import axios from 'axios';
-import { Descriptions, Space, Table, Tabs, Typography } from 'antd';
-import AWSTags from './aws_tags';
-import AWSNetworkInterfaces from './aws_network_interfaces';
+import React from 'react'
+import _ from 'lodash'
+import ItemsList from 'react-antd-itemslist'
+import { Descriptions, Space, Tabs, Tooltip } from 'antd'
+import AWSTags from './aws_tags'
+import AWSNetworkInterfaces from './aws_network_interfaces'
+import { vpcFilter, Copy } from './utils'
 
 export default class InstanceList extends React.Component {
-    itemsListUrl = "/api/instances"
+    itemsListUrl = "/api/instances" + vpcFilter(this.props.location.search)
+    itemBaseUrl = "/api/instances"
+
+    state = {
+        activeTabKey: "network",
+    }
 
     render() {
         return <ItemsList
             tableTitle="Instances"
             itemsListUrl={this.itemsListUrl}
-            itemBaseUrl={this.itemsListUrl}
-            dataKey="resource_id"
-            itemViewerWidth="60%"
+            itemBaseUrl={this.itemBaseUrl}
+            indexColViewLink={true}
             columns={this.getTableColumns()}
-            pagination={false}
-            rowActions={['deleteItem']}
+            pagination
+            dataKey="resource_id"
+            rowActions={[]}
+            rowSelection={false}
+            addButtonTitle={false}
+            deleteButtonTitle={false}
             itemViewer={this.renderSelectedItem}
+            itemViewerEditLink={false}
+            itemViewerWidth="60%"
             history={this.props.history}
         />
     }
@@ -34,6 +44,10 @@ export default class InstanceList extends React.Component {
             {
                 title: 'Id',
                 dataIndex: 'resource_id'
+            },
+            {
+                title: 'VPC',
+                dataIndex: 'vpc_id'
             },
             {
                 title: 'Launch Time',
@@ -80,16 +94,21 @@ export default class InstanceList extends React.Component {
     }
 
     renderSelectedItem = (details) => {
-        return <Space size="large" direction="vertical" style={{ width: "100%" }}>
+        return <Space size="middle" direction="vertical" style={{ width: "100%" }}>
             <Descriptions bordered size="small" column={1}>
-                <Descriptions.Item label="Name / Id">{details.name} / {details.resource_id}</Descriptions.Item>
-                <Descriptions.Item label="Region / AZ">{details.region} / {details.az}</Descriptions.Item>
-                <Descriptions.Item label="Account">{details.account_id}</Descriptions.Item>
-                <Descriptions.Item label="Instance Type">{details.instance_type}</Descriptions.Item>
-                <Descriptions.Item label="Key Name">{details.key_name}</Descriptions.Item>
-                <Descriptions.Item label="Launch Time / State">{details.launch_time.$date} / {details.state}</Descriptions.Item>
+                <Descriptions.Item label="Name / Id">
+                    <Copy text={details.name} /> / <Copy text={details.resource_id} /></Descriptions.Item>
+                <Descriptions.Item label="Region / AZ">
+                    <Copy text={details.region} /> / <Copy text={details.az} />
+                </Descriptions.Item>
+                <Descriptions.Item label="Account"><Copy text={details.account_id} /></Descriptions.Item>
+                <Descriptions.Item label="Instance Type"><Copy text={details.instance_type} /></Descriptions.Item>
+                <Descriptions.Item label="Key Name"><Copy text={details.key_name} /></Descriptions.Item>
+                <Descriptions.Item label="Launch Time / State">
+                    {details.launch_time.$date} / {details.state}
+                </Descriptions.Item>
             </Descriptions>
-            <Tabs>
+            <Tabs activeKey={this.state.activeTabKey} onChange={this.setActiveTabKey}>
                 <Tabs.TabPane tab="Network Interfaces" key="network">
                     <AWSNetworkInterfaces interfaces={details.network_interfaces} />
                 </Tabs.TabPane>
@@ -98,5 +117,9 @@ export default class InstanceList extends React.Component {
                 </Tabs.TabPane>
             </Tabs>
         </Space>
+    }
+
+    setActiveTabKey = (activeKey) => {
+        this.setState({ activeTabKey: activeKey })
     }
 }
