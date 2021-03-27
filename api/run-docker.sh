55c9f7d0736b4efd745e08c9677f9875a95c67b6
Kiran Madabhushi <maskiran@gmail.com>
2021-03-26 17:28:39 -0700

    docker command fixed, requirements file fixed with mongoengine

--- api/run-docker.sh
+++ api/run-docker.sh
@@ -4,4 +4,5 @@ docker run \
     --network container:aws-vpc-ui \
     -v $PWD:/svc \
     --workdir /svc \
-    python bash start-server.sh
+    python \
+    bash -c 'pip3 install -r requirements.txt && gunicorn rest:app --reload -w 2 -b 127.0.0.1:5000'
--- api/start-server.sh
+++
@@ -1,5 +0,0 @@
-# this script is for starting the service inside docker
-
-python3 -m venv /venv
-/venv/bin/pip install -r requirements.txt
-/venv/bin/gunicorn rest:app --reload -w 2 -b 127.0.0.1:5000
