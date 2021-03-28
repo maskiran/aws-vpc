3ba4d8941ed8424b5974e1e1d38a5da4f68526c9
Kiran Madabhushi <maskiran@gmail.com>
2021-03-28 16:37:39 -0700

    show recrawl vpc as the page header

--- ui/src/page_header.js
+++ ui/src/page_header.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import { HomeOutlined } from '@ant-design/icons';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { ReloadOutlined } from '@ant-design/icons'
+import { Button, Menu, notification, Spin } from 'antd'
+import axios from 'axios'
+import { vpcFilter, getResponseErrorMessage } from './utils'
 
+export default class PageHeader extends React.Component {
+    state = {
+        vpcDetails: {},
+        crawling: false,
+        vpcId: vpcFilter(this.props.location.search, false),
+    }
 
-class PageHeader extends React.Component {
-    navItems = [
-    ]
+    componentDidMount() {
+        this.getVpcDetails()
+    }
+
+    componentDidUpdate(prevProps) {
+        var newVpcId = vpcFilter(this.props.location.search, false)
+        var oldVpcId = vpcFilter(prevProps.location.search, false)
+        if (oldVpcId != newVpcId) {
+            this.setState({ vpcId: newVpcId })
+        }
+    }
 
     render() {
-        return <>
-            <Link to="/vpcs"><HomeOutlined /></Link>
-        </>
+        if (this.state.vpcId) {
+            return <Menu mode="horizontal" theme="dark">
+                <span style={{ float: "right" }}>
+                    <Button type="primary" icon={<ReloadOutlined />}
+                        disabled={this.state.crawling}
+                        onClick={this.recrawlVpc}>
+                        Re-Crawl VPC Resources
+                        {this.state.crawling && <Spin style={{ marginLeft: "16px" }} />}
+                    </Button>
+                </span>
+            </Menu >
+        } else {
+            return null
+        }
+    }
+
+    getVpcDetails = () => {
+        if (this.state.vpcId) {
+            axios.get('/api/vpcs/' + this.state.vpcId).then(rsp => {
+                this.setState({ vpcDetails: rsp.data })
+            })
+        }
+    }
+
+    recrawlVpc = () => {
+        var url = '/api/crawl/' + this.state.vpcDetails.region + '/' + this.state.vpcDetails.vpc_id
+        this.setState({ crawling: true })
+        axios.get(url).then(rsp => {
+        }).catch(err => {
+            var msg = getResponseErrorMessage(err)
+            notification.error({ duration: 0, message: msg })
+        }).finally((rsp) => {
+            this.setState({ crawling: false })
+        })
     }
 }
-
-export default PageHeader;
