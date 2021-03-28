b24b040cd424a9bda87c5c4f39cd2bc1eedf8519
Kiran Madabhushi <maskiran@gmail.com>
2021-03-28 16:43:05 -0700

    set worker timeout to 0

--- api/run-docker.sh
+++ api/run-docker.sh
@@ -6,4 +6,4 @@ docker run \
     -v $PWD:/svc \
     --workdir /svc \
     python \
-    bash -c 'pip3 install -r requirements.txt && gunicorn app:app --reload -w 2 -b 127.0.0.1:5000'
+    bash -c 'pip3 install -r requirements.txt && gunicorn app:app --reload -w 2 -t 0 -b 127.0.0.1:5000'
