42c7f409eb211488d7e852abb523654050cd0946
Kiran Madabhushi <maskiran@gmail.com>
2021-03-29 00:33:19 -0700

    return crawl count

--- api/apiresources/crawl.py
+++ api/apiresources/crawl.py
@@ -8,5 +8,4 @@ app = Blueprint('crawl', __name__)
 
 @app.route('/<region>/<vpc_id>')
 def crawl(region, vpc_id):
-    sync_resources(region, vpc_id)
-    return 'done'
+    return sync_resources(region, vpc_id)
