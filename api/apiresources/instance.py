e6b2a6e8e08d4ffae84f5d2182c0d4d61ac115e2
Kiran Madabhushi <maskiran@gmail.com>
2021-03-31 13:58:49 -0700

    return security group rules for each interface in the instance

--- api/apiresources/instance.py
+++ api/apiresources/instance.py
@@ -13,4 +13,10 @@ def instance_list():
 @app.route('/<resource_id>')
 def instance(resource_id):
     item = db.get_item(models.Instance, resource_id=resource_id)
+    # for all the interfaces get the security group rules
+    for intf in item['network_interfaces']:
+        for sg in intf['security_groups']:
+            sg_details = db.get_item(models.SecurityGroup, resource_id=sg['resource_id'])
+            sg['ingress_rules'] = sg_details['ingress_rules']
+            sg['egress_rules'] = sg_details['egress_rules']
     return item
