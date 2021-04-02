1ccd6678bfdf6ee1e8cd3546a32c0364cfc6157a
Kiran Madabhushi <maskiran@gmail.com>
2021-04-02 08:24:58 -0700

    run dev command

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
 docker stop awsvpc || true
-docker run --rm -p 9000:80 -p 9443:443 --name awsvpc -e MONGODB_HOST=mongodb://mongodb/aws --network db awsvpc
+docker run --rm -p 10002:80 --name awsvpc \
+    -e MONGODB_HOST=mongodb://mongo/aws -d \
+    --network db \
+    awsvpc
+
